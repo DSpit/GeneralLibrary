8707389f6d1dcb3b74cd5a764505ea7e9fb6f454
David Boivin ~Spit <db2_hockey@hotmial.com>
2015-10-03 19:18:24 -0400

    ~D/ Started essential player implementation

--- GeneralLibrary/src/com/dspit/musicplayer/Player.java
+++ GeneralLibrary/src/com/dspit/musicplayer/Player.java
@@ -2,4 +2,9 @@ package com.dspit.musicplayer;
 
 public class Player {
 
+	protected static final PlayingQueue SQueue = new PlayingQueue();
+	
+	public Player(){
+		
+	}
 }
--- GeneralLibrary/src/com/dspit/musicplayer/PlayingQueue.java
+++ GeneralLibrary/src/com/dspit/musicplayer/PlayingQueue.java
@@ -1,5 +1,123 @@
 package com.dspit.musicplayer;
 
-public class PlayingQueue {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
 
+
+public class PlayingQueue implements Queue<Song>{
+	 
+	
+	
+	protected PlayingQueue(){
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<Song> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends Song> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean add(Song e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean offer(Song e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Song remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Song poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Song element() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Song peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
