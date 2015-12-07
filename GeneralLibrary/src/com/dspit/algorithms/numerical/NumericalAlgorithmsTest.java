b21c9b668c785b7fcf3ad49ce2da8e75a929083d
David Boivin ~Spit <db2_hockey@hotmial.com>
2015-12-06 19:02:40 -0500

    ~D // wrote a ranged version of #linearCongruentialGenerator and wrote a test mathod for it

--- GeneralLibrary/src/com/dspit/algorithms/numerical/NumericalAlgorithmsTest.java
+++ GeneralLibrary/src/com/dspit/algorithms/numerical/NumericalAlgorithmsTest.java
@@ -19,9 +19,17 @@ public final class NumericalAlgorithmsTest {
 	public static void main(String[] args){
 		NumericalAlgorithmsTest test = new NumericalAlgorithmsTest();
 		
-		test.randomLinearCongruentialGenerator(50, 35, 409, 234, 100);
+/*		test.randomLinearCongruentialGenerator(50, 35, 409, 234, 100);
 		test.randomLinearCongruentialGenerator(50, 0, 409, 234, 100);
 		test.randomLinearCongruentialGenerator(50, 3, 409, 234, 100);
+		test.randomLinearCongruentialGenerator(50, 3, 409, 235, 2); //NOTE: generates alternating 0 and 1 because all multiplication and addition is being done by odd numbers.
+		test.randomLinearCongruentialGenerator(50, 2, 402, 232, 2);	//NOTE: only generates 0 because all multiplication and addition is being done by even numbers.
+*/		
+		for(int i = 0; i < 5; ++i){
+			test.randomRangedLinearCongruentialGenerator(100, i, 508, 67, 100, 20, 30);
+		}
+		
+		test.randomLinearCongruentialGenerator(100, 6, 508, 67, 100);
 	}
 	
 	public static void randomLinearCongruentialGenerator(int testSequenceLength, int testSeed, int testCMulti, int testCAdd, int testMod){
@@ -79,4 +87,41 @@ public final class NumericalAlgorithmsTest {
 		System.out.println();
 	}
 	
+	public static void randomRangedLinearCongruentialGenerator(int testSequenceLength, int testSeed, int testCMulti, int testCAdd, int testMod, int minValue, int maxValue){
+
+		//introduce
+			System.out.println("Ranged Linear Congruential Generator Test for Random Numbers:");
+			
+			//initialized all variables for more efficient testing and accurate elapsed time calculations
+			int[] output;
+			long startTime;
+			long elapsedTime;
+		
+			//start test
+			System.out.println("Sequence Length: " + testSequenceLength + "\n" +
+									"Seed: " + testSeed+ "\nBegin Generating...");
+			startTime = System.nanoTime();
+			output = Random.linearCongruentialGenerator(testSequenceLength, testSeed, testCMulti, testCAdd, testMod, minValue, maxValue);
+			elapsedTime = System.nanoTime() - startTime;
+			System.out.println("...End\n" + 
+								"Total Time Taken: " + elapsedTime + "ns ("
+								+ TimeUnit.MICROSECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS) + "micros)");
+				
+			//read out output and test
+			System.out.println("Output: " + Arrays.toString(output));
+			
+			//range check
+			System.out.println("Min Value: " + minValue + "\nMax Value: " + maxValue);
+			boolean check = true;
+			for(int i = 0; i < testSequenceLength; ++i){
+				if(output[i] < minValue){
+					System.out.print(" ( MIN" +">" + output[i] + ") ");
+					check = false;
+				}else if(output[i] > maxValue){
+					System.out.print(" (" + output[i] + "> MAX ) ");
+					check = false;
+				}
+			}
+			System.out.println("Range Check : " + check);
+	}
 }
--- GeneralLibrary/src/com/dspit/algorithms/numerical/Random.java
+++ GeneralLibrary/src/com/dspit/algorithms/numerical/Random.java
@@ -21,7 +21,7 @@ public final class Random {
 	 * also pretty predictable, though it has the feature of being able to 
 	 * reproduce a sequence. Useful for testing and repeated queries which need 
 	 * to find a given input. This algorithm also comes with the feature of being
-	 * able to cap its the randomly generated numbers.<br><br>
+	 * able to cap its the randomly generated numbers using the formula cap = mod - 1.<br><br>
 	 * The pitfalls of this algorithm is that after a given point the sequence is
 	 * bound to repeat. This is the nature of the modulo function. This also means
 	 * that not all the values between 0 and modulo will be generated before the
@@ -31,26 +31,65 @@ public final class Random {
 	 * @param seed	The starting value from which to begin the pseudo-random sequence.
 	 * @param cMulti Constant multiple.
 	 * @param cAdd Constant addition.
-	 * @param maxValue The max value of that can be generated. Value is used to modulo
+	 * @param mod The max value of that can be generated. Value is used to modulo
 	 * 			the whole sequence.
 	 * 
 	 * @return A sequence of pseudo-random values which are generated from the
 	 * 				parameters.
 	 */
-	public static int[] linearCongruentialGenerator(int sequenceLength, int seed, int cMulti, int cAdd, int maxValue){
+	public static int[] linearCongruentialGenerator(int sequenceLength, int seed, int cMulti, int cAdd, int mod){
 		//initializations
 		int[] rSequence = new int[sequenceLength];
 		
 		//fill output sequence with pseudo-random sequence
-		rSequence[0] = (cMulti*seed+cAdd) % maxValue;
+		rSequence[0] = (cMulti*seed+cAdd) % mod;
 		for(int i = 1; i < sequenceLength; ++i){
-			rSequence[i] = (cMulti*rSequence[i-1]+ cAdd) % maxValue;
+			rSequence[i] = (cMulti*rSequence[i-1]+ cAdd) % mod;
 		}
 		
 		return rSequence; 
 	}
 	
+	/**
+	 * This method makes use of the {@link #linearCongruentialGenerator(int, int, int, int, int)} to generate a list 
+	 * of values which are then processed by the formula :<br><br>
+	 * 
+	 * <b>result = min + (sequence# / mod) * (max-min)</b><br><br>
+	 * 
+	 * which is a formula that ensures that the result is still as fair as the generated number. 
+	 * <br><br><b>NOTE:</b> That the min and max are included in the range
+	 * 
+	 * @param minValue The minimum value that this algorithm should generate (included)
+	 * @param maxValue The maximum value that this algorithm should generate (included)
+	 * @param sequenceLength used in {@link #linearCongruentialGenerator()}
+	 * @param seed used in {@link #linearCongruentialGenerator()}
+	 * @param cMulti used in {@link #linearCongruentialGenerator()}
+	 * @param cAdd used in {@link #linearCongruentialGenerator()}
+	 * @param mod used in {@link #linearCongruentialGenerator()}
+	 * 
+	 * @return A sequence of pseudo-random values which are generated from the
+	 * 				parameters. These values are pseudo-randomly chosen from within
+	 * 				the range of the minimum value and maximum value parameters.
+	 */
+	public static int[] linearCongruentialGenerator(int sequenceLength, int seed, int cMulti, int cAdd, int mod, int minValue, int maxValue){
+		//generate a sequence of random numbers between 0 and mod.
+		int[] rSequence = Random.linearCongruentialGenerator(sequenceLength, seed, cMulti, cAdd, mod);
+		
+		//using the generated values, and making sure to keep the result mathematically fair, make the result
+		//of each value generate a ratio between 0 and 1. This will make sure that the multiplication of the
+		//range will result in a number which is within that range and can be added to the minimum value. Note that
+		//the randomness of this algorithm is entirely dependent on the randomness of the sequence generator,
+		//in this case, a Linear Congruential Generator.
+		for(int i = 0; i < sequenceLength; ++i){
+			rSequence[i] = minValue + Math.round((float)rSequence[i]/(float)(mod)*(maxValue-minValue));
+		}
+		
+		return rSequence;
+	}
+	
 	public static int[] multiLinearCongeuntialGenerator(int sequenceLength, int seed, int cMultiInit, int cAddInit){ //TODO multiple random number generator
 		return null;
 	}
+	
+	
 }
